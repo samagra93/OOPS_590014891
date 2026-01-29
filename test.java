130c921b353173be0a87a567a86c1b1e794f8a98
Samagra Gupta <gsamagra93@gmail.com>
2026-01-29 08:52:48 +0530

    Implemented inheritance using super() keyword in Java

---
+++ test.java
@@ -0,0 +1,34 @@
+class Car
+{
+    int n;
+    Car (int price)
+        {
+            this.n = price;
+        }
+        void show()
+        {
+            System.out.println(n);
+        }
+}
+class BMW extends Car
+{
+    int m;
+    BMW(int price,int updatedPrice)
+    {
+        super(price);
+        this.n=updatedPrice;
+    }
+    void f1()
+    {
+        System.out.println(n);
+    }
+}
+ 
+
+class test {
+    public static void main(String[] args) {
+        BMW c1 = new BMW(4,10);
+        c1.f1();
+
+    }
+}
